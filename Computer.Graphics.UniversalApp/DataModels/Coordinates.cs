31897dc4167e6fb838731dbae6152e083e9ea539
Eugene El <lomashonok@inbox.lv>
2019-09-17 07:41:31 +0300

    New project Data models

---
+++ Computer.Graphics.UniversalApp/DataModels/Coordinates.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computer.Graphics.UniversalApp.DataModels
+{
+    public class Coordinates
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+    }
+}
---
+++ Computer.Graphics.UniversalApp/DataModels/Scene.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computer.Graphics.UniversalApp.DataModels
+{
+    public class Scene
+    {
+        // Window configuration
+        public int WindowWidth { get; set; }
+        public int WindowHeight { get; set; }
+
+        // World configuration
+        public float WorldLeft { get; set; }
+        public float WorldRight { get; set; }
+        public float WorldBottom { get; set; }
+        public float WorldUp { get; set; }
+
+        // Content
+        public List<Coordinates> Coordinates { get; set; }
+        public List<int> IndexesSequence { get; set; }
+    }
+}
